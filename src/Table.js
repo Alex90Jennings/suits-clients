5991c934900fc6a24dc8ea868644a5aaa2b5509e
Alex Jennings <97463171+Alex90Jennings@users.noreply.github.com>
2022-08-01 17:36:52 +0200

    added table for more than 2 players

--- src/Table.js
+++ src/Table.js
@@ -1,23 +1,18 @@
-import RenderTable from "./RenderTable"
-import { useContext, useState } from "react";
+import TableForMoreThanTwoPlayers from './TableForMoreThanTwoPlayers.js'
+import TableForTwoPlayers from './TableForTwoPlayers'
+import { useContext } from "react";
 import { globalContext } from './helper/globalContext';
 
-
-function Table() {
-  const { lobbyCode } = useContext(globalContext)
-  // const [cardDeck, setCardDeck] = useState([])
-
-  // if (gameState = "generate card deck") {
-
-  // }
-
-
-  return (
-    <>
-      <h2>Welcome to Table {lobbyCode}</h2>
-      <RenderTable />
-    </>
-  );
+function Table () {
+    const { playerList, lobbyCode } = useContext(globalContext)
+    
+    return (
+        <>
+            <h2>Welcome to Table {lobbyCode}</h2>
+            {playerList.length === 2 (<TableForTwoPlayers />)}
+            {playerList.length > 2 (<TableForMoreThanTwoPlayers />)}
+        </>
+    )
 }
 
 export default Table;
---
+++ src/TableForMoreThanTwoPlayers.js
@@ -0,0 +1,131 @@
+import RenderCards from "./RenderCards.js";
+import { useContext } from "react";
+import { globalContext } from './helper/globalContext';
+
+
+function TableForMoreThanTwoPlayers() {
+  const { playerList } = useContext(globalContext)
+
+
+  return (
+    <>
+      <div className="table-rectangle center">
+        <div className="three-rows-expand-one-three m-left-l">
+          <div></div>
+          <ul className="center grid-template-game">
+            {playerList.length > 2 && (
+            <li id="player3" className="card">
+              <p>{playerList[2].username}</p>
+              <img className="animal center" src="../assets/diagrams/india/2.png" alt="elephant"></img>
+              <ul className="responsive-columns">
+                  <li>
+                      <img className="trick" src="./india/card-back.png" alt="card-back"></img>
+                  </li>
+                  <li>
+                      <img className="trick" src="./india/card-back.png" alt="card-back"></img>
+                  </li>
+              </ul>
+              <img className="playing-card center" src="./india/red_joker.png" alt="card"></img>
+            </li>
+            )}
+            {playerList.length > 3 && (
+              <li id="player4" class="card">
+                <p>{playerList[3].username}</p>
+                <img class="animal center" src="../assets/diagrams/india/3.png" alt="cobra"></img>
+                <div class="three-rows-expand-one-three">
+                    <div></div>
+                    <ul class="responsive-columns">
+                        <li>
+                            <img class="trick" src="./india/card-back.png" alt="card-back"></img>
+                        </li>
+                        <li>
+                            <img class="trick" src="./india/card-back.png" alt="card-back"></img>
+                        </li>
+                    </ul>
+                    <div></div>
+                </div>
+                <img class="playing-card center" src="./india/red_joker.png" alt="card"></img>
+              </li>
+            )}
+            {playerList.length > 4 && (
+              <li id="player5" class="card">
+              <p>{playerList[4].username}</p>
+              <img class="animal center" src="../assets/diagrams/india/4.png" alt="monkey"></img>
+              <div class="three-rows-expand-one-three">
+                  <div></div>
+                  <ul class="responsive-columns">
+                      <li>
+                          <img class="trick" src="./india/card-back.png" alt="card-back"></img>
+                      </li>
+                      <li>
+                          <img class="trick" src="./india/card-back.png" alt="card-back"></img>
+                      </li>
+                  </ul>
+                  <div></div>
+              </div>
+              <img class="playing-card center" src="./india/red_joker.png" alt="card"></img>
+            </li>             
+            )}
+          </ul>
+        </div>
+        <div className="three-rows-expand-one-three m-left-l">
+          <div></div>
+          <ul className="center grid-template-game">
+            <li id="player2" className="card">
+              <p>{playerList[1].username}</p>
+              <img className="animal center" src="../assets/diagrams/india/1.png" alt="cow"></img>
+              <ul className="responsive-columns">
+                  <li>
+                      <img className="trick" src="./india/card-back.png" alt="card-back"></img>
+                  </li>
+                  <li>
+                      <img className="trick" src="./india/card-back.png" alt="card-back"></img>
+                  </li>
+              </ul>
+              <img className="playing-card center" src="./india/red_joker.png" alt="card"></img>
+            </li>
+            <li id="player1" class="card">
+              <p>{playerList[0].username}</p>
+              <img class="animal center" src="../assets/diagrams/india/0.png" alt="tiger"></img>
+              <div class="three-rows-expand-one-three">
+                  <div></div>
+                  <ul class="responsive-columns">
+                      <li>
+                          <img class="trick" src="./india/card-back.png" alt="card-back"></img>
+                      </li>
+                      <li>
+                          <img class="trick" src="./india/card-back.png" alt="card-back"></img>
+                      </li>
+                  </ul>
+                  <div></div>
+              </div>
+              <img class="playing-card center" src="./india/red_joker.png" alt="card"></img>
+            </li>
+            {playerList.length > 5 && (
+              <li id="player6" class="card">
+                <p>{playerList[6].username}</p>
+                <img class="animal center" src="../assets/diagrams/india/5.png" alt="crocodile"></img>
+                <div class="three-rows-expand-one-three">
+                    <div></div>
+                    <ul class="responsive-columns">
+                        <li>
+                            <img class="trick" src="./india/card-back.png" alt="card-back"></img>
+                        </li>
+                        <li>
+                            <img class="trick" src="./india/card-back.png" alt="card-back"></img>
+                        </li>
+                    </ul>
+                    <div></div>
+                </div>
+                <img class="playing-card center" src="./india/red_joker.png" alt="card"></img>
+              </li>             
+            )}
+          </ul>
+        </div>
+      </div>
+      <RenderCards />
+    </>
+  );
+}
+
+export default TableForMoreThanTwoPlayers;
