97dcc85ef2ef9ea00403abb8e9dbc6c781d6764a
Alex Jennings <aj0312@my.bristol.ac.uk>
2022-09-24 22:32:43 +0200

    added pseudo code

--- src/SideSection.js
+++ src/SideSection.js
@@ -5,6 +5,23 @@ import client from './utils/client.js';
 function SideSection () {
     const { lobbyCode, playerList, refreshTable, gameState, setGameState, roundId, setTrick, decideWhoPlaysNext } = useContext(globalContext)
 
+    /*GAME STATES 
+        8 cards dealt to each player
+        Players bet
+        Bets revealed
+        GAME STARTS 
+            First card played
+            Next player plays card
+            Repeat until all players have played
+            Decide who wins the hand
+            Repeat the hand, until there are 0 cards left
+            Allocate scores based on bet vs actual hands won
+        REDUCE THE NUMBER OF CARDS BY 1
+            Repeat the game, until there are number of cards = 0
+        */
+        
+
+
     const checkIfEveryoneHasBet = () => {
         let playersWhoHaveBet = 0
         client
